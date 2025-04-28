0b07fa161df76db52203a6e3ea3fe464bc2c976f
sashahuzii <rapace1301@icloud.com>
2025-04-28 16:00:20 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# puet
+# Untitled
+
+A Pen created on CodePen.
+
+Original URL: [https://codepen.io/sashahuzii/pen/JoorERG](https://codepen.io/sashahuzii/pen/JoorERG).
+
---
+++ src/script.js
@@ -0,0 +1,2 @@
+// This is a simple JS file for future functionality, like product filtering, adding to cart, etc.
+// Currently, it's empty but you can expand it as needed for more interactivity.
